ecef5568e1afd9827941a72621e8d6de050c1611
cmitchell <clmitchell630@gmail.com>
2019-08-13 02:19:25 -0500

    JS correctly pushes submit information to database

--- assets/javascript/trainapp.js
+++ assets/javascript/trainapp.js
@@ -1,5 +1,11 @@
 //Pseudo
-//page will draw information from firebase 
+//page will draw information from firebase and update the page with stored data
+//form will accept inputs from user, when submit is clicked.... magic
+//magic part 1 = inputs will be captured and form will be reset.
+//magic part 2 = captured inputs will be sent to the database
+//magic part 3 = page will dynamically update with information from database
+
+//set up time math and Moment.js
 
 $(function () {
 
@@ -15,7 +21,41 @@ $(function () {
     };
 
     firebase.initializeApp(firebaseConfig);
+    //------variables-------
+    var database = firebase.database();
 
+    //form submit button
+    $("#submit").on("click", dataPush);
+
+    //database listener
+    database.ref().on("child_added", function(snapshot){
+        var name = snapshot.val().name;
+        var city = snapshot.val().city;
+        var time = snapshot.val().time;
+        var freq = snapshot.val().frequency;
+    }
     //------functions------
 
+    //database push
+    function dataPush(e){
+        e.preventDefault();
+
+        var name = $("#name").val().trim();
+        var city = $("#city").val().trim();
+        var time = moment($("#time").val().trim(), "hh:mm").format("hh:mm");
+        var freq = $("#freq").val().trim();
+
+        database.ref().push({
+            name: name,
+            city: city,
+            time: time,
+            frequency: freq
+        });
+
+        $("#name").val("");
+        $("#city").val("");
+        $("#time").val("");
+        $("#freq").val("");
+
+    }
 });
