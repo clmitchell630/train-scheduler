6b84c16760c962351314b6a6cc94a0780b573590
cmitchell <clmitchell630@gmail.com>
2019-08-13 01:17:33 -0500

    HTML CSS set up webpage, JS set up firebase reference

---
+++ assets/javascript/trainapp.js
@@ -0,0 +1,21 @@
+//Pseudo
+//page will draw information from firebase 
+
+$(function () {
+
+    // firebase setup
+    const firebaseConfig = {
+        apiKey: "AIzaSyDI5gmTb2_YLj95bvO27wvumaa_-AiWHYk",
+        authDomain: "train-scheduler-unit7.firebaseapp.com",
+        databaseURL: "https://train-scheduler-unit7.firebaseio.com",
+        projectId: "train-scheduler-unit7",
+        storageBucket: "train-scheduler-unit7.appspot.com",
+        messagingSenderId: "809687632300",
+        appId: "1:809687632300:web:ac7d559563bc1f5a"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
+    //------functions------
+
+});
